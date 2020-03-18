f64bb4efc1aefa4e212af22e956b3481238ced40
nightlordtw <nightlordtw@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2020-03-18 09:57:06 +0000

    Added unit tests for ccalc based on flagged issues by Anneke
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/metamisc@549 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/tests/testthat/test_valmeta_0.R
+++ pkg/tests/testthat/test_valmeta_0.R
@@ -33,4 +33,25 @@ test_that("Standard error of transformed c-statistic", {
 })
 
 
+test_that("Anneke", {
+  
+  # Create test dataset provided by Anneke Damen
+  testdat <- data.frame(Authoryear = c("Andersson 2015", "Chia 2014", "Goff 2014", "Goff 2014", "Jung 2015"),
+                   Samplesize = c(3396, 307, 5041, 735, 114622),
+                   N_events = c(284, 22, 539, 107, 7669),
+                   Cstat = c(0.72, 0.55, 0.6843, 7669, 0.727),
+                   Cstat95LB = c(NA, NA, NA, NA, 0.721),
+                   Cstat95UB = c(NA, NA, NA, NA, 0.734),
+                   CstatSE = c(0.014, 0.050, NA, NA, NA))
+  
+  ad <- ccalc(cstat = Cstat, cstat.cilb = Cstat95LB, cstat.ciub = Cstat95UB, 
+              cstat.se = CstatSE, N = Samplesize, o = N_events, data = testdat, 
+              slab = Authoryear)
+  
+  expect_equal(as.character(ad["Jung 2015","theta.se.source"]), "Confidence Interval")
+  expect_equal(ad["Jung 2015","theta.cilb"], subset(testdat, Authoryear == "Jung 2015")$Cstat95LB)
+  expect_equal(ad["Jung 2015","theta.ciub"], subset(testdat, Authoryear == "Jung 2015")$Cstat95UB)
+  
+})
+
 
